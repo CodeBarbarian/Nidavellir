47822ac8cd0da1eefaf86edc60782ea2941fc160
Morten Haugstad <morten.haugstad@gmail.com>
2024-02-23 17:40:03 +0100

    Project for another day, rest api client codename "Murdoc".

---
+++ src/Core/Forge/Middleware/Restful/Client/Murdoc.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Core\Forge\Middleware\Restful\Client;
+
+class Murdoc {
+	/**
+	 * This is going to be a rest api client
+	 *
+	 * We should support the following functions:
+	 * GET
+	 * POST
+	 * PUT
+	 * PATCH
+	 * DELETE
+	 * COPY
+	 * HEAD
+	 * OPTIONS
+	 * LINK
+	 * UNLINK
+	 * PURGE
+	 * LOCK
+	 * UNLOCK
+	 * PROPFIND
+	 * VIEW
+	 *
+	 * Typical Features include the ability to manipulate the body, header, pre request scripts, tests.
+	 */
+}
